26bec8ee5d72bc06837b6a25cfe036144bc1547a
Akifa Khan <110725258+AkifaKhan@users.noreply.github.com>
2024-11-27 19:07:02 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,9 @@
-from flask import Flask, render_template
-import mysql.connector
-
+from flask import Flask
 app = Flask(__name__)
 
-config = {
-    'host': 'cloudways-server.mysql.database.azure.com',
-    'user': 'lxpfgntzwv',
-    'password': 'Blueberry@2001',  # Replace with your actual password
-    'database': 'cloudways-database',  # Replace with your actual database name
-    'ssl_disabled': False  # SSL is required
-}
-
 @app.route('/')
-def index():
-    try:
-        # try to establish a connection
-        cnx = mysql.connector.connect(**config)
+def home():
+    return "Hello, World!"
 
-        # Check if the connection is successful
-        if cnx.is_connected():
-            print("Connection Successful")
-
-            cursor = cnx.cursor()
-            cursor.execute("SELECT * FROM Dimaircraft")  # Fetch all rows from the table
-            users = cursor.fetchall()
-            cursor.close()  # Close cursor
-            cnx.close()  # Close the connection
-
-            return render_template('index.html', aircrafts=users)
-        else:
-            return "Connection Failed", 500
-
-    except mysql.connector.Error as e:
-        return f"Error Connecting to MySQL Database: {e}", 500
-
-    finally:
-        if cnx and cnx.is_connected():
-            cnx.close()
-        
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
